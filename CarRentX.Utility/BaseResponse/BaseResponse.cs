ebf8cb7ffd63980643670a3ca18865922b9c96fa
hyusufoglu <hyusufoglu@isbak.istanbul>
2023-06-08 10:04:27 +0300

    Business katmanına Service ve Manager oluşturuldu. Core katmanına Utility projesi eklenerek BaseResponse sınıfı oluşturuldu.

---
+++ CarRentX.Utility/BaseResponse/BaseResponse.cs
@@ -0,0 +1,18 @@
+﻿namespace CarRentX.Utility.BaseResponse
+{
+	public class BaseResponse<TData>
+	{
+		public bool Status { get; set; }
+		public string? Message { get; set; }
+		public TData? Data { get; set; }
+
+		public static BaseResponse<TData> Success(TData data, string message)
+		{
+			return new BaseResponse<TData> {Status = true, Message = message, Data = data};
+		}
+		public static BaseResponse<TData> Error(string message)
+		{
+			return new BaseResponse<TData> { Status = false, Message = message };
+		}
+	}
+}
